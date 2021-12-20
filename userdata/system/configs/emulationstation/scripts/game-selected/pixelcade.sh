ac2e066833c9d098831e2ac55dd6aabeea72381b
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-19 20:39:55 -0500

    Update pixelcade.sh

--- userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
+++ userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-JAVA_HOME=/userdata/jdk/ /userdata/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelcade.jar -m stream -c ${1} -g ${2) -t "${3}"
+JAVA_HOME=/userdata/jdk/ /userdata/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelcade.jar -m stream -c ${1} -g ${2} -t "${3}"
