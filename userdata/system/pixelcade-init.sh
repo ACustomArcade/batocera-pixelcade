69ba27fb5cdb8165299b271627efcd895b4382d4
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-26 12:26:59 -0500

    Update pixelcade-init.sh

--- userdata/system/pixelcade-init.sh
+++ userdata/system/pixelcade-init.sh
@@ -4,12 +4,12 @@ export JAVA_HOME=/userdata/jdk/
 
 case "$1" in
 start)
-  /userdata/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelweb.jar -b &
+  /userdata/system/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelweb.jar -b &
   sleep 3
   curl http://127.0.0.1:8080/arcade/stream/user/batocera
 ;;
 stop)
-  /userdata/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelcade.jar -q
+  /userdata/system/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelcade.jar -q
 ;;
 esac
 exit $?
