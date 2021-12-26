876e6ecc9e242217cc5580fb10005665080d3062
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-26 12:44:50 -0500

    Update pixelcade.sh

--- userdata/system/configs/emulationstation/scripts/system-selected/pixelcade.sh
+++ userdata/system/configs/emulationstation/scripts/system-selected/pixelcade.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-curl http://127.0.0.1:8080/console/stream/${1}?t=${2}
+
+system="${1}"
+systemname="${2}"
+
+curl -G \
+        --data-urlencode "t=${systemname}" \
+        http://127.0.0.1:8080/console/stream/${system}
