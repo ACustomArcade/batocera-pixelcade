497a31a773e236d6535703deb6c3962b377f4f50
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-26 12:42:14 -0500

    Update pixelcade.sh

--- userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
+++ userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "${1} ${2} ${3}" >> /userdata/system/logs/test.log
+system="${1}"
+rom="${2}"
+romname="${3}"
+
+if [[ "${system}" == "fbneo" ]]; then
+        system="mame"
+fi
+
 curl -G \
-        --data-urlencode "t=${3}" \
-        http://127.0.0.1:8080/arcade/stream/${1}/`basename ${2}`
+        --data-urlencode "t=${romname}" \
+        http://127.0.0.1:8080/arcade/stream/${system}/`basename ${rom}`
