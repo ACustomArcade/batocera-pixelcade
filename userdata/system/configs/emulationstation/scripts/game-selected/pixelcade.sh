7e46edb5f2eacdaea274dd731399c69891a6eb51
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-28 12:12:01 -0500

    Remove .scummvm from rom name

--- userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
+++ userdata/system/configs/emulationstation/scripts/game-selected/pixelcade.sh
@@ -8,6 +8,15 @@ if [[ "${system}" == "fbneo" ]]; then
         system="mame"
 fi
 
+case ${system} in
+  fbneo)
+    system="mame"
+    ;;
+  scummvm)
+    rom="${rom%.*}"
+    ;;
+esac
+
 curl -G \
         --data-urlencode "t=${romname}" \
         http://127.0.0.1:8080/arcade/stream/${system}/`basename ${rom}`
