cc67d860e4b2957908921c90441f84d4ce12f405
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-30 13:52:11 -0500

    Remove initial call to set batocera logo

--- userdata/system/pixelcade-init.sh
+++ userdata/system/pixelcade-init.sh
@@ -4,11 +4,7 @@ export JAVA_HOME=/userdata/jdk/
 
 case "$1" in
 start)
-  /userdata/system/jdk/jre/bin/java -Dioio.SerialPorts=ttyACM0:ttyAMA0 -jar /userdata/system/pixelcade/pixelweb.jar -b &
-  sleep 3
-  curl -G \
-    --data-urlencode "event=FEStart" \
-    http://127.0.0.1:8080/arcade/stream/user/batocera
+  /userdata/system/jdk/jre/bin/java -Dioio.SerialPorts=/dev/ttyACM0 -jar /userdata/system/pixelcade/pixelweb.jar -b &
 ;;
 stop)
   curl -G \
