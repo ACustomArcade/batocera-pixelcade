24544d10a25e537bb9b2a9c9454074b435cffcb6
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-30 10:32:42 -0500

    Add FEStart and FEQuit events

--- userdata/system/pixelcade-init.sh
+++ userdata/system/pixelcade-init.sh
@@ -6,10 +6,14 @@ case "$1" in
 start)
   /userdata/system/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelweb.jar -b &
   sleep 6
-  curl http://127.0.0.1:8080/arcade/stream/user/batocera
+  curl -G \
+    --data-urlencode "event=FEStart" \
+    http://127.0.0.1:8080/arcade/stream/user/batocera
 ;;
 stop)
-  /userdata/system/jdk/jre/bin/java -jar /userdata/system/pixelcade/pixelcade.jar -q
+  curl -G \
+    --data-urlencode "event=FEQuit" \
+    http://127.0.0.1:8080/quit
 ;;
 esac
 exit $?
